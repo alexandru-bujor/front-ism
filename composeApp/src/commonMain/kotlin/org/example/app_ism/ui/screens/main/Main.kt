5dd7862099e3e9e7c7220661500bffc8b8e9c0b2
Alexandru Bujor <alexandru.bujor@isa.utm.ms>
2026-03-23 02:14:48 +0200

    Main.kt update

--- composeApp/src/commonMain/kotlin/org/example/app_ism/ui/screens/main/Main.kt
+++ composeApp/src/commonMain/kotlin/org/example/app_ism/ui/screens/main/Main.kt
@@ -1,30 +1,17 @@
 package org.example.app_ism.ui.screens.main
-
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.material3.Button
+import org.example.app_ism.ui.screens.auth.AuthScreen
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
 import cafe.adriel.voyager.core.screen.Screen
-import app_ism.composeapp.generated.resources.Res
-import app_ism.composeapp.generated.resources.compose_multiplatform
-import org.example.app_ism.Greeting
+import cafe.adriel.voyager.navigator.Navigator
+import org.example.app_ism.ui.screens.home.HomeScreen
 
 class MainScreen : Screen {
 
     @Composable
     override fun Content() {
         MaterialTheme {
-
-            Text("Hello World")
-
+            Navigator(HomeScreen)
         }
     }
 }
